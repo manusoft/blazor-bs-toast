59387895192545146149f38b2f91b7823bd370df
Manojbabu@Home <manuae2019@outlook.com>
2025-03-18 16:47:14 +0400

    Added new wasm and remove old wasm.

--- test/Test.Blazor.Wasm/Program.cs
+++ test/Test.Blazor.Wasm/Program.cs
@@ -1,13 +1,12 @@
 using ManuHub.Blazor.Toast;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Test.Blazor.Wasm.Standalone;
+using Test.Blazor.Wasm;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddBlazorToast();
 
 await builder.Build().RunAsync();
